400a853beea82a80a2cd7dfc22e1072cc28746b0
Navneet Kaur <navneetkaur0889@gmail.com>
2025-01-26 17:57:05 +0000

    setting up

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import React from "react";
+
+// Import your global CSS file
+import "../global.css";
+
+export default Slot;
+
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-200">
+      <Text className="text-2xl text-center">Home</Text>
+    </View>
+  );
+}
